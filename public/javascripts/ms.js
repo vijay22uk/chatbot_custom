0e0a8353a4377cc69e0d55a0c62c940e416361d2
vijay pandey <vpandey@merrillcorp.com>
2018-02-12 22:57:50 +0530

    added string

---
+++ public/javascripts/ms.js
@@ -0,0 +1,6 @@
+BotChat.App({
+    directLine: { secret: 'pEmeg3MEGlE.cwA.HeU.zOXwBT8NSiZbHslM9XwJMI3bPHpxMvdGS503AIq5SiM' },
+    user: { id: 'userid' },
+    bot: { id: '39525ee8-f6bf-4590-a037-57c203bb24b5' },
+    resize: 'detect'
+}, document.getElementById("bot"));
